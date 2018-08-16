09a5e93147c8c6bf6476af50599c5de30ba5bbe8
Fedya <gamefeget@gmail.com>
2018-08-16 05:09:46 -0400

    xuiv jope

--- change_php.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for res in $(cat users.txt | awk -F/ {'print $3'});
-do for ver in 4.4 5.1 5.2 5.3 5.4 5.5 5.6 7.0 7.1;
-        do /usr/bin/piniset --replace="log_errors:Off" --version=$ver --user=$res ;
-        done;
-echo "$res done...";
-done
--- new.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WORD=$(ps aufx | grep "python" | awk {'print  $10'} > time.txt &&  rev time.txt | cut -c 4- | rev)
-VARIABLE=${WORD:0:1};
-NVARIABLE=${WORD:0:5};
-echo $VARIABLE;
-echo $NVARIABLE;
-echo $WORD;
