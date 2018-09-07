8f616a9d60839bdb21eafd845ad92f11af39e0be
root <root@letstryit.ml>
2018-09-07 05:09:09 +0200

    fix 1 issue

--- src/test/java/com/playtika/autest/FirTest.java
+++ src/test/java/com/playtika/autest/FirTest.java
@@ -2,7 +2,6 @@ package com.playtika.autest;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
